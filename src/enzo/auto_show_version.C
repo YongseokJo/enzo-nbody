88e466b93fdc701c3db95ce3fca9b9e217dab6de
Yongseok Jo <g.kerex@gmail.com>
2022-08-08 15:39:39 +0900

    gitignore

--- src/enzo/auto_show_version.C
+++ src/enzo/auto_show_version.C
@@ -2,6 +2,6 @@
 void auto_show_version(FILE *fp) {
    fprintf (fp,"\n");
    fprintf (fp,"Git Branch   master\n");
-   fprintf (fp,"Git Revision 99fa953d2631e35178b2f55bb5c52c91b036b958\n");
+   fprintf (fp,"Git Revision ae13163d3b6e882e5168a605c00bb61ed75a9200\n");
    fprintf (fp,"\n");
 }
