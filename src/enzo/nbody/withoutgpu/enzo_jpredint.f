2f9929a40f252f5bc6b028107b8ba3e91c6b4707
sykim <mintlux@snu.ac.kr>
2022-08-23 15:40:11 +0900

    fortran only code complete

--- src/enzo/nbody/withoutgpu/enzo_jpredint.f
+++ src/enzo/nbody/withoutgpu/enzo_jpredint.f
@@ -1,27 +1,31 @@
-      SUBROUTINE JPRED_INT(I,X,XDOT,F,FD,SCALE,DT,NMAX,NXTLEN
-     &     XP,XPDOT)
+      SUBROUTINE JPRED_INT(SCALE,DT,NXTLEN)
 *
 *
 *       Prediction of single particle, used in intgrt.F
 *       ----------------------------------------
 *       Common variables (inputs and outputs)
 
-      INTEGER I,NMAX,NXTLEN
-      REAL*8  X(3,NMAX),XDOT(3,NMAX),F(3,NMAX),FD(3,NMAX),
-      REAL*8  BODY(NMAX),SCALE,DT
-      REAL*8  XP(3),XPDOT(3)
+      INCLUDE 'enzo_common6.h'
+      INTEGER I,NXTLEN
+      REAL*8  SCALE,DT
 
       S = DT
       S1 = 1.5*S
       S2 = 2.0*S
 
 *     Note X may become corrected value X0 for zero interval.
-      XP(1) = ((FDOT(1,I)*S + F(1,I))*S + XDOT(1,I))*S + X(1,I)
-      XP(2) = ((FDOT(2,I)*S + F(2,I))*S + XDOT(2,I))*S + X(2,I)
-      XP(3) = ((FDOT(3,I)*S + F(3,I))*S + XDOT(3,I))*S + X(3,I)
-      XPDOT(1) = (FDOT(1,I)*S1 + F(1,I))*S2 + XDOT(1,I)
-      XPDOT(2) = (FDOT(2,I)*S1 + F(2,I))*S2 + XDOT(2,I)
-      XPDOT(3) = (FDOT(3,I)*S1 + F(3,I))*S2 + XDOT(3,I)
+      DO I=1,NXTLEN
+      
+         X(1,I) = ((F0DOT(1,I)*S + F0(1,I))*S + X0DOT(1,I))*S + X0(1,I)
+         X(2,I) = ((F0DOT(2,I)*S + F0(2,I))*S + X0DOT(2,I))*S + X0(2,I)
+         X(3,I) = ((F0DOT(3,I)*S + F0(3,I))*S + X0DOT(3,I))*S + X0(3,I)
+         XDOT(1,I) = (F0DOT(1,I)*S1 + F0(1,I))*S2 + X0DOT(1,I)
+         XDOT(2,I) = (F0DOT(2,I)*S1 + F0(2,I))*S2 + X0DOT(2,I)
+         XDOT(3,I) = (F0DOT(3,I)*S1 + F0(3,I))*S2 + X0DOT(3,I)
+
+*         PRINT *,'POSITION',X(1,I),XDOT(1,I)
+
+      END DO
 *
       RETURN
 *
