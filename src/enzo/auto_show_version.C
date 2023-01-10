6bdb1c80274780b405b1cc0254080ea577d583ec
Yongseok Jo <yjo10@rustyamd2.cm.cluster>
2023-01-09 21:08:17 -0500

    working on makefile

--- src/enzo/auto_show_version.C
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-void auto_show_version(FILE *fp) {
-   fprintf (fp,"\n");
-   fprintf (fp,"Git Branch   seperateComm\n");
-   fprintf (fp,"Git Revision cb8b94c22fbef2939d95892df2cae571133eb96f\n");
-   fprintf (fp,"\n");
-}
