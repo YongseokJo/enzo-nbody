bf0f0b19237f01ea7164a9ea1a07c3cc4dd2935d
Yongseok Jo <yjo10@rustyamd2.cm.cluster>
2023-01-19 19:27:49 -0500

    Objects file (.F) added

--- src/enzo/auto_show_version.C
+++ src/enzo/auto_show_version.C
@@ -2,6 +2,6 @@
 void auto_show_version(FILE *fp) {
    fprintf (fp,"\n");
    fprintf (fp,"Git Branch   master\n");
-   fprintf (fp,"Git Revision 374bd916ea5ddde75ec0d13c80693e46e10211d2\n");
+   fprintf (fp,"Git Revision 656e9cbdddf78a8c233c30b054e76e61d95e4ba3\n");
    fprintf (fp,"\n");
 }
