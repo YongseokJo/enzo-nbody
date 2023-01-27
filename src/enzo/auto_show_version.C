6a721658990deb6c0c1c48a87abd52d997c8801d
Yongseok Jo <yjo10@rustyamd2.cm.cluster>
2023-01-27 15:33:57 -0500

    nbody6.for is incorrect.

--- src/enzo/auto_show_version.C
+++ src/enzo/auto_show_version.C
@@ -2,6 +2,6 @@
 void auto_show_version(FILE *fp) {
    fprintf (fp,"\n");
    fprintf (fp,"Git Branch   master\n");
-   fprintf (fp,"Git Revision 60e308cb9c9a097e71165d26c4b74bde612b33fb\n");
+   fprintf (fp,"Git Revision e768493c2a13d6067ece5bcae14c55dbcf584522\n");
    fprintf (fp,"\n");
 }
