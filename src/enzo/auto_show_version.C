e0cc53e1c51aff1dab569760f89e0921e8871855
sykim <mintlux@snu.ac.kr>
2023-02-02 00:12:06 +0900

    compiled

--- src/enzo/auto_show_version.C
+++ src/enzo/auto_show_version.C
@@ -1,7 +1,9 @@
 #include <stdio.h>
 void auto_show_version(FILE *fp) {
+   fprintf (fp,"make[1]: Warning: File `DEPEND' has modification time 80 s in the future\n");
    fprintf (fp,"\n");
    fprintf (fp,"Git Branch   master\n");
-   fprintf (fp,"Git Revision 7ad074ad506fedc90232ca30cd6486eab782ec18\n");
+   fprintf (fp,"Git Revision d167f916f730ee8f4578336bcee6c0d44a8d1f97\n");
    fprintf (fp,"\n");
+   fprintf (fp,"make[1]: warning:  Clock skew detected.  Your build may be incomplete.\n");
 }
