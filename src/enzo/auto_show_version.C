d167f916f730ee8f4578336bcee6c0d44a8d1f97
Yongseok Jo <yjo10@rustyamd2.cm.cluster>
2023-01-31 21:11:16 -0500

    No omp test

--- src/enzo/auto_show_version.C
+++ src/enzo/auto_show_version.C
@@ -2,6 +2,6 @@
 void auto_show_version(FILE *fp) {
    fprintf (fp,"\n");
    fprintf (fp,"Git Branch   master\n");
-   fprintf (fp,"Git Revision dcf88da340632225ca6d7ec06e02df9530dd493b\n");
+   fprintf (fp,"Git Revision 7ad074ad506fedc90232ca30cd6486eab782ec18\n");
    fprintf (fp,"\n");
 }
