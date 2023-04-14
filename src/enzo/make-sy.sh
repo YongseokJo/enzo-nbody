0942402b06558c1ca2618a8f4f4780fc45217ca5
sykim <mintlux@snu.ac.kr>
2023-04-14 12:37:29 +0900

    disabled conf

---
+++ src/enzo/make-sy.sh
@@ -0,0 +1,24 @@
+#!/bin/sh  
+
+# This makes enzo.exe on Happiness@SNU
+
+echo "Making enzo on"
+pwd
+make clean
+make default
+cd ../../
+./configure
+cd src/enzo/
+
+module add icc
+module add mpi
+
+make machine-linux-sy
+make precision-64 integers-32 particle-id-32 max-baryons-30 opt-aggressive lcaperf-no max-tasks-per-node-36 grackle-yes new-problem-types-yes nbody-yes photon-no cuda-no
+make show-config
+make show-flags
+make -j3
+cp enzo.exe enzo_spare.exe
+echo "Make done!"
+pwd
+date
