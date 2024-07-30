91f0a817b4d6e970062bd91dcb1ff1cd12a48520
YongseokJo <g.kerex@gmail.com>
2024-07-30 18:32:24 -0400

    CUDA optimization by Minyong

--- src/enzo/nbody/cuda/cuda_kernels.h
+++ src/enzo/nbody/cuda/cuda_kernels.h
@@ -3,12 +3,14 @@
 #include "../defs.h"
 
 
-__global__	void initialize(CUDA_REAL* result, int* neighbor, int* num_neighbor, CUDA_REAL* diff, CUDA_REAL *magnitudes, int n, int m, int* subset);
+__global__	void initialize(CUDA_REAL* result, CUDA_REAL* diff, CUDA_REAL *magnitudes, int n, int m, int* subset);
 __global__ void compute_pairwise_diff_subset(const CUDA_REAL* ptcl, CUDA_REAL* diff, int n, int m, const int* subset);
-__global__ void compute_magnitudes_subset(const CUDA_REAL *r2, const CUDA_REAL* diff, CUDA_REAL* magnitudes, int n, int m, int* subset);
+__global__ void compute_magnitudes_subset(const CUDA_REAL *r2, const CUDA_REAL* diff, CUDA_REAL* magnitudes, int n, int m, int* subset, bool* neighbor2);
 __global__ void compute_forces_subset(const CUDA_REAL* ptcl, CUDA_REAL *diff, const CUDA_REAL* magnitudes, int n, int m, const int* subset);
 __global__ void assign_neighbor(int *neighbor, int* num_neighbor, const CUDA_REAL* r2, const CUDA_REAL* magnitudes, int n, int m, const int *subset);
 __global__ void reduce_forces(const CUDA_REAL *diff, CUDA_REAL *result, int n, int m);
+__global__ void print_forces_subset(CUDA_REAL* result, int m);
+void reduce_forces_cublas(cublasHandle_t handle, const CUDA_REAL *diff, CUDA_REAL *result, int n, int m);
 
 /*
 __device__ void _addition(Result &result, const Result res);
